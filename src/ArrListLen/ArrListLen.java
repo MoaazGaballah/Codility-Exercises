d694481906f06b03042a5d163db96f7bb24a3d64
Muaz <gaballah.moaaz@gmail.com>
2021-10-02 13:37:18 +0300

    O(N**2) solution

---
+++ src/ArrListLen/ArrListLen.java
@@ -0,0 +1,25 @@
+package ArrListLen;
+
+public class ArrListLen {
+
+    public static int solution(int[] A) {
+        // write your code in Java SE 8
+
+        int index = 0;
+        int count = 0;
+        for (int elemenet : A) {
+            count++;
+            if (A[index] == -1)
+                break;
+            index = A[index];
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int A[] = {1, 4, -1, 3, 2};
+
+        System.out.println(solution(A));
+    }
+}
---
+++ src/CountBoundedSlices/CountBoundedSlices.java
@@ -0,0 +1,53 @@
+package CountBoundedSlices;
+
+import java.util.Arrays;
+
+public class CountBoundedSlices {
+
+    public static class Slice {
+
+        int P;
+        int Q;
+
+        public Slice(int P, int Q){
+            this.P = P;
+            this.Q = Q;
+        }
+    }
+    public static int solution(int K, int[] A) {
+        // write your code in Java SE 8
+
+        int count = 0;
+        for (int P = 0; P < A.length; P++) {
+            for (int Q = P; Q < A.length; Q++) {
+                if (max(A, new Slice(P, Q)) - min(A, new Slice(P, Q)) <= K)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static int max(int [] A, Slice slice){
+        A = Arrays.copyOfRange(A, slice.P, slice.Q + 1);
+        Arrays.sort(A);
+        return A[A.length - 1];
+    }
+
+    public static int min(int [] A, Slice slice){
+        A = Arrays.copyOfRange(A, slice.P, slice.Q + 1);
+        Arrays.sort(A);
+        return A[0];
+    }
+
+    public static void main(String[] args) {
+
+        int A[] = {3, 5, 7, 6, 3};
+
+        System.out.println(solution(2, A));
+
+
+//        // test max and min methods
+//        System.out.println(max(A, new Slice(1, 3)));
+//        System.out.println(min(A, new Slice(1, 3)));
+    }
+}
